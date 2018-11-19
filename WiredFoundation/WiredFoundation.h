c0f537deeb3861e5ee8df20c955241eaea9ae3b8
Rafaël Warnault <rw@opale.pro>
2018-11-19 18:20:05 +0100

    Initial Commit

---
+++ WiredFoundation/WiredFoundation.h
@@ -0,0 +1,19 @@
+//
+//  WiredFoundation.h
+//  WiredFoundation
+//
+//  Created by Rafael Warnault on 19/11/2018.
+//  Copyright © 2018 Read-Write. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WiredFoundation.
+FOUNDATION_EXPORT double WiredFoundationVersionNumber;
+
+//! Project version string for WiredFoundation.
+FOUNDATION_EXPORT const unsigned char WiredFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WiredFoundation/PublicHeader.h>
+
+
