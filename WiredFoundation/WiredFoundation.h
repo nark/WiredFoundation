94ee9c830ac4e98ded31294120f4a9a6be77e63d
Rafaël Warnault <rw@opale.pro>
2018-11-19 18:40:06 +0100

    Initial commit

--- WiredFoundation/WiredFoundation.h
+++ WiredFoundation/WiredFoundation.h
@@ -1,19 +1,64 @@
-//
-//  WiredFoundation.h
-//  WiredFoundation
-//
-//  Created by Rafael Warnault on 19/11/2018.
-//  Copyright © 2018 Read-Write. All rights reserved.
-//
+/* $Id$ */
 
-#import <Cocoa/Cocoa.h>
+/*
+ *  Copyright (c) 2008-2009 Axel Andersson
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-//! Project version number for WiredFoundation.
-FOUNDATION_EXPORT double WiredFoundationVersionNumber;
+#import <WiredFoundation/WIObject.h>
 
-//! Project version string for WiredFoundation.
-FOUNDATION_EXPORT const unsigned char WiredFoundationVersionString[];
+#if TARGET_OS_MAC
+    #import <WiredFoundation/RegexKitLite.h>
+    #import <WiredFoundation/RegexKitLite-WIFoundation.h>
+#endif
 
-// In this header, you should import all the public headers of your framework using statements like #import <WiredFoundation/PublicHeader.h>
+#import <WiredFoundation/WIDateFormatter.h>
+#import <WiredFoundation/WIEventQueue.h>
+#import <WiredFoundation/WIMacros.h>
+#import <WiredFoundation/WIReadWriteLock.h>
+#import <WiredFoundation/WISettings.h>
+#import <WiredFoundation/WISizeFormatter.h>
+#import <WiredFoundation/WITextFilter.h>
+#import <WiredFoundation/WITimeIntervalFormatter.h>
+#import <WiredFoundation/WITypes.h>
+#import <WiredFoundation/WIURL.h>
 
-
+#import <WiredFoundation/NSArray-WIFoundation.h>
+#import <WiredFoundation/NSData-WIFoundation.h>
+#import <WiredFoundation/NSDate-WIFoundation.h>
+#import <WiredFoundation/NSDateComponents-WIFoundation.h>
+#import <WiredFoundation/NSDictionary-WIFoundation.h>
+#import <WiredFoundation/NSError-WIFoundation.h>
+#import <WiredFoundation/NSFileManager-WIFoundation.h>
+#import <WiredFoundation/NSInvocation-WIFoundation.h>
+#import <WiredFoundation/NSLocale-WIFoundation.h>
+#import <WiredFoundation/NSNetService-WIFoundation.h>
+#import <WiredFoundation/NSNotificationCenter-WIFoundation.h>
+#import <WiredFoundation/NSNumber-WIFoundation.h>
+#import <WiredFoundation/NSObject-WIFoundation.h>
+#import <WiredFoundation/NSProcessInfo-WIFoundation.h>
+#import <WiredFoundation/NSScanner-WIFoundation.h>
+#import <WiredFoundation/NSSet-WIFoundation.h>
+#import <WiredFoundation/NSString-WIFoundation.h>
+#import <WiredFoundation/NSThread-WIFoundation.h>
